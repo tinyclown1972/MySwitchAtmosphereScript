db6d75e317d14d29f76fcd43d118d8d4935c55bd
leonard <tinyclown1972@gmail.com>
2024-08-26 14:37:02 +0800

    Update switchScript.sh

--- switchScript.sh
+++ switchScript.sh
@@ -520,14 +520,14 @@ else
 fi
 
 ### Fetch lastest OC_Toolkit from https://github.com/halop/OC_Toolkit/releases/latest
-curl -sL https://api.github.com/repos/halop/OC_Toolkit/releases/latest \
+curl -sL https://api.github.com/repos/halop/OC_Toolkit_SC_EOS/releases/latest \
   | jq '.name' \
   | xargs -I {} echo {} >> ../description.txt
-curl -sL https://api.github.com/repos/halop/OC_Toolkit/releases/latest \
+curl -sL https://api.github.com/repos/halop/OC_Toolkit_SC_EOS/releases/latest \
   | grep -oP '"browser_download_url": "\Khttps://[^"]*kip.zip"' \
   | sed 's/"//g' \
   | xargs -I {} curl -sL {} -o kip.zip
-curl -sL https://api.github.com/repos/halop/OC_Toolkit/releases/latest \
+curl -sL https://api.github.com/repos/halop/OC_Toolkit_SC_EOS/releases/latest \
   | grep -oP '"browser_download_url": "\Khttps://[^"]*OC.Toolkit.u.zip"' \
   | sed 's/"//g' \
   | xargs -I {} curl -sL {} -o OC.Toolkit.u.zip
